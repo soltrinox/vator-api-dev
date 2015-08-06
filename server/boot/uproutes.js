b406823f0cb18635481e6dd3a54775aaefc96aa3
soltrinox <soltrinox@gmail.com>
2015-08-06 00:54:20 +0000

    image routes

--- server/boot/uproutes.js
+++ server/boot/uproutes.js
@@ -8,7 +8,7 @@ var StorageService = require('loopback-component-storage').StorageService;
 
 var providers = null;
   providers = require('../providers.json');
-
+var uuid = require('node-uuid');
 
 var handler = new StorageService(
   {
@@ -25,7 +25,7 @@ server.get('/uploadimage', function(req, res, next) {
   var form = "<html><body><h1>Storage Service Demo</h1>" +
     "<a href='/download'>List all containers</a><p>" +
     "Upload to container con1: <p>" +
-    "<form method='POST' enctype='multipart/form-data' action='/uploadimage/vatorprofilecache'>"
+    "<form method='POST' enctype='multipart/form-data' action='/uploadtest/vatorprofilecache'>"
     + "File to upload: <input type=file name=uploadedFiles multiple=true><br>"
     + "Notes about the file: <input type=text name=note><br>"
     + "<input type=submit value=Upload></form>" +
@@ -37,30 +37,108 @@ server.get('/uploadimage', function(req, res, next) {
 
 
 
-server.post('/uploadimage/:container', function(req, res, next) {
-var options = 
-{
-  container: 'vatorprofilecache'  ,
-  getFilename: function(fileInfo, req, res) {
+
+server.post('/uploadprofile/:container/:pid', function(req, res, next) {
+  var type = 'profile';
+  var options = 
+  {
+    container: 'vatorprofilecache'  ,
+    getFilename: function(fileInfo, req, res) {
         var origFilename = fileInfo.name;
         var parts = origFilename.split('.'),
             extension = parts[parts.length-1];
-return '-profile.' +  extension   ;
-       // return uuid.v4() + '-profile.' +  extension   ;
-  }
-};
+	//return '-'+ type +'.' +  extension   ;
+        return uuid.v4() + '-'+  type +'.' +  extension   ;
+    }
+  };
 
- console.log('POST: '+ JSON.stringify( options) );
-
+//  console.log('POST: '+ JSON.stringify( options) );
   handler.upload(req, res, options, function(err, result){
     if (!err) {
+
+      var responseMessage = {};
+	responseMessage.result = result.files.file[0];
+	responseMessage.pid = req.params.pid;
+	responseMessage.type = 'profilePic';
+        responseMessage.fields = req.params;
+
       res.setHeader('Content-Type', 'application/json');
-      res.status(200).send(result);
+      res.status(200).send(responseMessage);
     } else {
       res.status(500).send(err);
     }
   });
 });
+
+
+
+server.post('/uploadcover/:container/:pid', function(req, res, next) {
+  var type = 'cover';
+  var options =
+  {
+    container: 'vatorprofilecache'  ,
+    getFilename: function(fileInfo, req, res) {
+        var origFilename = fileInfo.name;
+        var parts = origFilename.split('.'),
+            extension = parts[parts.length-1];
+        //return '-'+ type +'.' +  extension   ;
+        return uuid.v4() + '-'+  type +'.' +  extension   ;
+    }
+  };
+
+//  console.log('POST: '+ JSON.stringify( options) );
+  handler.upload(req, res, options, function(err, result){
+    if (!err) {
+
+        var responseMessage = {};
+        responseMessage.result = result.files.file[0];
+        responseMessage.pid = req.params.pid;
+        responseMessage.type = 'coverPic';
+	responseMessage.fields = req.params;
+
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(responseMessage);
+
+    } else {
+      res.status(500).send(err);
+    }
+  });
+});
+
+
+server.post('/uploadmedia/:container/:pid', function(req, res, next) {
+  var type = 'media';
+  var options =
+  {
+    container: 'vatorprofilecache'  ,
+    getFilename: function(fileInfo, req, res) {
+        var origFilename = fileInfo.name;
+        var parts = origFilename.split('.'),
+            extension = parts[parts.length-1];
+        //return '-'+ type +'.' +  extension   ;
+        return uuid.v4() + '-'+  type +'.' +  extension   ;
+    }
+  };
+
+//  console.log('POST: '+ JSON.stringify( options) );
+  handler.upload(req, res, options, function(err, result){
+    if (!err) {
+
+        var responseMessage = {};
+        responseMessage.result = result.files.file[0];
+        responseMessage.pid = req.params.pid;
+        responseMessage.type = 'media';
+        responseMessage.fields = req.params;
+
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).send(responseMessage);
+
+    } else {
+      res.status(500).send(err);
+    }
+  });
+});
+
 
 
   server.use(router);
