94882dc7b91fcf209961901b4f462fdf0b5e8265
Dylan Rosario <soltrinox@gmail.com>
2015-08-07 02:57:45 -0700

    project

--- common/models/product.js
+++ common/models/product.js
@@ -2,13 +2,16 @@
 module.exports = function(Product, Team) {
 
   var tempTags = {};
+  var tempTeam = {};
 
 
   Product.observe('before save', function updateTimestamp(ctx, next) {
     if (ctx.instance) {
-      // ctx.instance.updated = new Date();
+      if(!ctx.instance.companyId || 0 === ctx.instance.companyId ){
+
+      }
       tempTags = ctx.instance.tags;
-      console.log('BEFORE Tags %j', tempTags);
+      console.log('BEFORE INSTANCE Tags %j', tempTags);
     } else {
       // ctx.data.updated = new Date();
       tempTags = ctx.data.tags;
@@ -29,6 +32,53 @@ module.exports = function(Product, Team) {
     }
     next();
   });
+
+
+  // get the whole product team etc
+
+
+  Product.getEntireProduct = function(id,cb) {
+    var app = Product.app;
+    var tapp = Team.app;
+    Product.findById(id,  function(err, tapp, product) {
+      console.log('PRODUCT: %j',product);
+      // links the object
+      if(err) {
+        console.log(err);
+      } else {
+        product.teams({ productId:id },function(err, tapp, teams){
+          var teamz = [];
+          // for(obj in teams){
+          angular.forEach(teams, function(value, key) {
+            console.log('TEAM: %j',value);
+            var teamId = value.id;
+            tapp.getEntireCompany({ id:teamId },function(err, team){
+                teamz.push(team);
+            });
+          });
+
+          var response = {
+                details: product,
+                teams : teamz
+          };
+          cb(null, response);
+
+        });
+      }
+    });
+  };
+
+
+  Team.remoteMethod('getEntireProduct', {
+    accepts: [{arg: 'id', type: 'string'}],
+    returns: {arg: 'company', type: 'object'},
+    http: {path:'/entirecompany/:id', verb: 'get'}
+  });
+
+
+
+
+
 
 
 };
