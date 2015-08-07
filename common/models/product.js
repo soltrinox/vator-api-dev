22bfad2ed76faff6cd58a731e7d836d21eec1bf1
Dylan Rosario <soltrinox@gmail.com>
2015-08-07 01:59:40 -0700

    tags updates

--- common/models/product.js
+++ common/models/product.js
@@ -1,14 +1,30 @@
 'use strict';
 module.exports = function(Product, Team) {
 
-  var tempTeam = {};
+  var tempTags = {};
 
 
   Product.observe('before save', function updateTimestamp(ctx, next) {
     if (ctx.instance) {
       ctx.instance.updated = new Date();
+      tempTags = ctx.instance.tags;
     } else {
       ctx.data.updated = new Date();
+      tempTags = ctx.data.tags;
+    }
+    console.log('BEFORE Tags %j', tempTags);
+    next();
+  });
+
+  Product.observe('after save', function(ctx, next) {
+    if (ctx.instance) {
+      console.log('Saved Product %s#%s', ctx.Model.modelName, ctx.instance.id);
+      console.log('AFTER SAVE Tags %j', tempTags);
+    } else {
+      console.log('Updated %s matching %j',
+        ctx.Model.pluralModelName,
+        ctx.where);
+        console.log('AFTER UPDATE Tags %j', tempTags);
     }
     next();
   });
