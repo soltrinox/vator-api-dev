f413de7abde3f089ef655017316d73768bcfb597
Dylan Rosario <soltrinox@gmail.com>
2015-08-10 08:10:51 -0700

    vatorX objects

---
+++ common/models/group.js
@@ -0,0 +1,69 @@
+'use strict';
+module.exports = function(Group) {
+
+
+  // Group.getEntireCompany = function(id,cb) {
+  //   var app = Team.app;
+  //   Team.findById(id,  function(err, team) {
+  //     // links the object
+  //     if(err) {
+  //       console.log(err);
+  //     } else {
+  //       team.members({ teamId:id },function(err, profiles){
+  //         team.projects({ teamId:id },function(err, products){
+  //               // Team.media({ teamId:id },function(err, media){
+  //               var media  = { files : [ 'http://bit.ly/1IuNOek', 'http://bit.ly/1M9PmiD' ]};
+  //               // ----- compile object for response  -----
+  //               var response = {
+  //                     details: team,
+  //                     members : profiles,
+  //       				      projects : products,
+  //                     media: media
+  //       			  };
+  //               cb(null, response);
+  //           // });
+  //         });
+  //       });
+  //     }
+  //   });
+  // };
+  //
+  //
+  // Team.remoteMethod('getEntireCompany', {
+  //   accepts: [{arg: 'id', type: 'string'}],
+  //   returns: {arg: 'company', type: 'object'},
+  //   http: {path:'/entirecompany/:id', verb: 'get'}
+  // });
+  //
+  // Team.getPartCompany = function(id,cb) {
+  //   var app = Team.app;
+  //   Team.findById(id,  function(err, team) {
+  //     // links the object
+  //     if(err) {
+  //       console.log(err);
+  //     } else {
+  //       team.members({ teamId:id },function(err, profiles){
+  //               var media  = { files : [ 'http://bit.ly/1IuNOek', 'http://bit.ly/1M9PmiD' ]};
+  //               // ----- compile object for response  -----
+  //               var response = {
+  //                     details: team,
+  //                     members : profiles,
+  //                     media: media
+  //               };
+  //               cb(null, response);
+  //       });
+  //     }
+  //   });
+  // };
+  //
+  //
+  // Team.remoteMethod('getPartCompany', {
+  //   accepts: [{arg: 'id', type: 'string'}],
+  //   returns: {arg: 'team', type: 'object'},
+  //   http: {path:'/partcompany/:id', verb: 'get'}
+  // });
+
+
+
+
+};
