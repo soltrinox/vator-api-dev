0b670ba2ec99c9c65bd8d311a78be61134e3df27
Dylan Rosario <soltrinox@gmail.com>
2015-08-14 11:11:42 -0700

    dates

--- common/models/investment.js
+++ common/models/investment.js
@@ -3,13 +3,13 @@ module.exports = function(Investment) {
 
   Investment.observe('before save', function setDefaultUsername(ctx, next) {
     if (ctx.instance) {
-      if(!ctx.instance.created){
+      if(!ctx.instance.created || ctx.instance.created.length === 0){
         ctx.instance.created = Date.now();
       }
-      if(!ctx.instance.date){
+      if(!ctx.instance.date || ctx.instance.date.length === 0){
         ctx.instance.date = Date.now();
       }
-      if(!ctx.instance.exitdate){
+      if(!ctx.instance.exitdate || ctx.instance.exitdate.length === 0){
         ctx.instance.exitdate = Date.now();
       }
       ctx.instance.updated = Date.now();
