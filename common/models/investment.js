04725b6b0e368040407622c04fc4b9d097fca36c
Dylan Rosario <soltrinox@gmail.com>
2015-08-14 11:37:38 -0700

    fx

--- common/models/investment.js
+++ common/models/investment.js
@@ -5,23 +5,10 @@ module.exports = function(Investment) {
     if (ctx.instance) {
 
       console.log(JSON.stringify(ctx.instance));
-
-
-      if(ctx.instance.created &&  ctx.instance.created === ''){
+      if(!ctx.instance.created ){
         console.log('NO CREATED DATE');
         ctx.instance.created = Date.now();
       }
-      if(ctx.instance.date  && ctx.instance.date === ''){
-        console.log('NO START DATE');
-        ctx.instance.date = Date.now();
-      }
-      if(ctx.instance.exitdate && ctx.instance.exitdate === ''){
-        console.log('NO EXIT DATE');
-        ctx.instance.exitdate = Date.now();
-      }
-      // ctx.instance.created = Date.now();
-      // ctx.instance.date = Date.now();
-      // ctx.instance.exitdate = Date.now();
       ctx.instance.updated = Date.now();
     }
     next();
