4c364ed128d716245d4ea989e6d0c5d312e4045c
Dylan Rosario <soltrinox@gmail.com>
2015-08-14 11:19:08 -0700

    da

--- common/models/investment.js
+++ common/models/investment.js
@@ -4,13 +4,15 @@ module.exports = function(Investment) {
   Investment.observe('before save', function setDefaultUsername(ctx, next) {
     if (ctx.instance) {
       if(!ctx.instance.created || ctx.instance.created.length === 0 || ctx.instance.created === ''){
-        console.log();
+        console.log('NO CREATED DATE');
         ctx.instance.created = Date.now();
       }
       if(!ctx.instance.date || ctx.instance.date.length === 0 || ctx.instance.date === ''){
+        console.log('NO START DATE');
         ctx.instance.date = Date.now();
       }
       if(!ctx.instance.exitdate || ctx.instance.exitdate.length === 0 || ctx.instance.exitdate === ''){
+        console.log('NO EXIT DATE');
         ctx.instance.exitdate = Date.now();
       }
       ctx.instance.updated = Date.now();
