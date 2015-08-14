899a01f8de0dc60283b7d60bebe5bd97f09279dd
Dylan Rosario <soltrinox@gmail.com>
2015-08-14 11:32:46 -0700

    ff

--- common/models/investment.js
+++ common/models/investment.js
@@ -7,21 +7,21 @@ module.exports = function(Investment) {
       console.log(JSON.stringify(ctx.instance));
 
 
-      if(!ctx.instance.created || ctx.instance.created.length === 0 || ctx.instance.created === ''){
+      if((ctx.instance.created && ctx.instance.created.length === 0) || ctx.instance.created === ''){
         console.log('NO CREATED DATE');
         ctx.instance.created = Date.now();
       }
-      if(!ctx.instance.date || ctx.instance.date.length === 0 || ctx.instance.date === ''){
+      if(ctx.instance.date || ctx.instance.date.length === 0 || ctx.instance.date === ''){
         console.log('NO START DATE');
         ctx.instance.date = Date.now();
       }
-      if(!ctx.instance.exitdate || ctx.instance.exitdate.length === 0 || ctx.instance.exitdate === ''){
+      if(ctx.instance.exitdate || ctx.instance.exitdate.length === 0 || ctx.instance.exitdate === ''){
         console.log('NO EXIT DATE');
         ctx.instance.exitdate = Date.now();
       }
-      ctx.instance.created = Date.now();
-      ctx.instance.date = Date.now();
-      ctx.instance.exitdate = Date.now();
+      // ctx.instance.created = Date.now();
+      // ctx.instance.date = Date.now();
+      // ctx.instance.exitdate = Date.now();
       ctx.instance.updated = Date.now();
     }
     next();
